3f2f41cfb76cd0afc554583a2ee366ff3d1fd560
Mareli Koh <2509267@upy.edu.mx>
2026-06-11 18:27:58 -0600

    CW07: Add sequence multiplication logic

--- CW07/digito_verificador.py
+++ CW07/digito_verificador.py
@@ -1,3 +1,9 @@
 def calcular_digito_verificador(rol):
     rol_str = str(rol)
     rol_invertido = rol_str[::-1]
+    secuencia = [2, 3, 4, 5, 6, 7]
+    suma = 0
+    
+    for i, digito in enumerate(rol_invertido):
+        multiplicador = secuencia[i % len(secuencia)]
+        suma += int(digito) * multiplicador
